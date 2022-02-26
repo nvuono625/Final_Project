ba732357954892b2ffe61c86020d7d075a66c8e6
dlescarbeau413 <dlescarbeau413@gmail.com>
2022-02-26 18:27:00 -0500

    final project schema outline

---
+++ final_project_db.sql
@@ -0,0 +1,27 @@
+Create table Patients (
+ 	id integer NOT NULL,
+ 	gender text NOT NULL,
+ 	age float NOT NULL,
+ 	hypertension integer NOT NULL,
+ 	heart_disease integer NOT NULL,
+	ever_married text NOT NULL,
+ 	work_type text NOT NULL,
+ 	residence_type text NOT NULL,
+	PRIMARY KEY (id)
+ 	);
+	
+Create Table results (
+	id integer,
+	avg_glucose_level float,
+ 	bmi float,
+ 	smoking_status text,
+ 	stroke integer 
+);
+
+
+
+Select* 
+From patients
+
+SELECT*
+FROM results
